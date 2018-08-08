93df5953df3b7539e3413b1d753b7f926a6c21f8
Rubens Farias <rubens.mbfarias@gmail.com>
2018-08-08 11:12:40 +0100

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,9 +1,21 @@
-# Annoying NVIM opening inside itself.
-function nvimm
+function reload
+  source ~/.config/fish/config.fish
+end
+
+# Install nvr first!
+function nvim
   if test -z $NVIM_LISTEN_ADDRESS
-    /usr/bin/nvim $argv
+    /usr/local/bin/nvim $argv
   else
-    set -l currp (pwd)
-    python -c "from neovim import attach; file = '$argv[-1]'; file = file if file.startswith('/') else '$currp/'+file; nvim = attach('socket', path='$NVIM_LISTEN_ADDRESS'); nvim.comm
+    which nvr > /dev/null; and nvr --remote-tab $argv
   end
 end
+
+alias gai "git add go/src/improbable.io"
+alias gcm "git commit -m"
+alias grb "git rebase master"
+alias gup "git push --set-upstream origin (git symbolic-ref --short HEAD)"
+alias gpf "git push --force --set-upstream origin (git symbolic-ref --short HEAD)"
+alias gco "git checkout"
+alias gst "git stash"
+alias gsp "git stash pop"
